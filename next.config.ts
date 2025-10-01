079b2fe4046444a38e6eafe133f319ae20cccef4
Mhmd0Mhmod <mohamedmahmoudf.2002@gmail.com>
2025-10-01 13:15:27 +0300

    feat: initialize OpenNext Cloudflare for development in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
+initOpenNextCloudflareForDev();
 export default nextConfig;
