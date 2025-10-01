ecdfdd352a892e0e69812a26e50b956642dcb958
Mhmd0Mhmod <mohamedmahmoudf.2002@gmail.com>
2025-10-01 19:00:08 +0300

    chore: remove Cloudflare initialization from next.config.ts; delete wrangler configuration files; update package-lock.json for peer dependency

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
-if (process.env.NODE_ENV === "development") {
-  initOpenNextCloudflareForDev();
-}
+
 export default nextConfig;
