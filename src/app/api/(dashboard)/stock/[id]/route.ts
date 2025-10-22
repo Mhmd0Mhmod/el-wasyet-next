cf70a06b712a8c7e8a92b286bc8eccb4771660d4
Mhmd0Mhmod <mohamedmahmoudf.2002@gmail.com>
2025-10-22 10:27:33 +0300

    feat(api): update stock retrieval logic to handle async params and improve error handling

--- src/app/api/(dashboard)/stock/[id]/route.ts
+++ src/app/api/(dashboard)/stock/[id]/route.ts
@@ -4,15 +4,19 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   try {
+    const { id } = await params;
+    if (!id) {
+      return new NextResponse("Bad Request", { status: 400 });
+    }
     const { data } = await fetch.get<StockItem[]>("/Stock/getall", {
       headers: {
         Authorization: `Bearer ${request.headers.get("Authorization")}`,
       },
     });
-    const stockItem = data.find((item) => item.branchId === Number(params.id));
+    const stockItem = data.find((item) => item.branchId === Number(id));
     if (!stockItem) {
       return new NextResponse("Not Found", { status: 404 });
     }
